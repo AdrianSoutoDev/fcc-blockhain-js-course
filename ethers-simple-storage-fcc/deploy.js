720c4af26bb117cfbc4b0a28ae1c3553e5633a51
Adrián Souto <adrian.souto@udc.es>
2023-03-11 18:22:40 +0100

    Add extra secutiry to private keys

--- ethers-simple-storage-fcc/deploy.js
+++ ethers-simple-storage-fcc/deploy.js
@@ -8,8 +8,18 @@ async function main() {
   // );
 
   const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  /*
+  //this is ffor production to work with more security with the private keys
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
+  let wallet = ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+
+  wallet = await wallet.connect(provider);
+  */
   const abi = fs.readFileSync(
     "./compiled/SimpleStorage_sol_SimpleStorage.abi",
     "utf-8"
